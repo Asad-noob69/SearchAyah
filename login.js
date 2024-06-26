cee81a58bc526874ff604bc9715dabf064107158
Asad <asadaliabbasi787@gmail.com>
2024-06-25 21:01:43 -0700

    login popup design

---
+++ login.js
@@ -0,0 +1,20 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('loginModal');
+    const btn = document.getElementById('loginBtn');
+    const span = document.getElementsByClassName('close')[0];
+
+    btn.onclick = function() {
+        modal.style.display = 'block';
+    }
+
+    span.onclick = function() {
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+});
