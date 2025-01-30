8d669538bc5fc795e86679d1c3be9e47a3870c21
Asad_noob69 <asadaliabbasi787@gmail.com>
2025-01-30 17:48:06 +0500

    good

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
       <Header />
       <main className="flex-grow">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-          <h1>testing</h1>
           <QuranSearch />
           
         </div>
